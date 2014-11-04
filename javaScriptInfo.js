cc269b859bd5ac3b30bb8e521154c44260ba0062
Leisy Moliner <lhmoliner@gmail.com>
2014-11-04 15:48:12 -0700

    Changes for register page/CSS
    -updated register page
    -added javascript funtions
    -added CSS stylesheet

--- javaScriptInfo.js
+++ javaScriptInfo.js
@@ -17,10 +17,59 @@
 
 
 //register page
+/*
+Student:Leisy Moliner Hernandez
+Course Module: CPRG210
+Date: October 30,2014
+Assignment: Project Workshop HTML/CSS/Javascript 
+*/
 //Arrays
 
 //Functions
+function verify_all()
+{   
+    var ok = true;
+    verify_postal();
+    verify_home_phone();
+    verify_business_phone();
+    verify_email();
+    return ok;
+}
 
+function verify_postal()
+{
+  var regex=new RegExp("^([A-Za-z][0-9]){3}$","g"); //long style
+  var x=document.info.postal.value;
+  if(!regex.test(x)) {alert('Please enter a correct postal code');ok=false;}
+}
+
+function verify_home_phone()
+{
+    var regex=/^[0-9]{10}$/;  //short style
+    var x=document.info.home_phone.value;
+    if(!regex.test(x)) {alert('Please enter a valid home phone number');ok=false;}
+}
+
+function verify_business_phone()
+{
+    var regex=/^[0-9]{10}$/;  //short style
+    var x=document.info.business_phone.value;
+    if(!regex.test(x)) {alert('Please enter a valid business phone number');ok=false;}
+}
+
+function verify_email()
+{
+    var regex=/@/;  //short style
+        var x=document.info.email.value;
+        if(!regex.test(x)) {alert('Please enter a valid email');ok=false;}
+}
+
+function verify_password()
+{
+    if ((document.info.password.value.length < 3) || (document.info.password.value.length > 8))
+    {alert('Please enter a valid password');ok=false;}
+  
+}
 
 
 //order page
--- register.php
+++ register.php
@@ -1,5 +1,8 @@
 <!--
-
+Student:Leisy Moliner Hernandez
+Course Module: CPRG210
+Date: October 30,2014
+Assignment: Project Workshop HTML/CSS/Javascript 
 -->
 
 
@@ -12,7 +15,7 @@
         <link rel="stylesheet" type="text/css" href="stylesheet.css"><!--css stylesheet-->
         <script type="text/javascript" src="javaScriptInfo.js"></script><!--javascript functions-->
         
-        <title>Main Page</title>
+        <title>Customer Registration</title>
              
     </head>
 
@@ -25,7 +28,7 @@
                 
                 <!-- header content goes here --> 
                 <?php
-                    $title = "welcome to Travel Experts";
+                    $title = "register with us";
                     include("header.php");
                 ?>
             
@@ -36,7 +39,7 @@
                 
                 <!-- nav content goes here --> 
                 <?php
-                    include("nav.php");
+                    include("navigation.php");
                 ?>
                 
             </div><!-- nav ends --> 
@@ -44,7 +47,135 @@
 
             <div id="section"><!-- section starts -->
                 
-                <!-- section content goes here --> 
+                <br /><br />
+                <form name="info" onsubmit="return verify_all();" method="get" action="bouncer.php">
+                     
+                    <!--- Row One --->
+                    <div02>
+                        <p style="color:cyan"><b>Contact Information</b></p>
+                        <hr><!---Horizontal rule --->
+                        <table>
+                            <col width="200px">
+                            <col width="15px">
+                            <col width="350px">
+
+                            <tr> 
+                                <td class="alignRight">First Name</td>
+                                <td>-</td>
+                                <td><input type="text" name="first_name" size="45px"></td>
+                            </tr>
+                            <tr> 
+                                <td class="alignRight">Last Name</td>
+                                <td>-</td>
+                                <td><input type="text" name="last_name" size="45px"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Address</td>
+                                <td>-</td>
+                                <td><input type="text" name="address" size="45px"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">City</td>
+                                <td>-</td>
+                                <td><input type="text" name="city" size="45px"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Province</td>
+                                <td>-</td>
+                                <td><input type="text" name="province" size="45px"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Country</td>
+                                <td>-</td>
+                                <td><input type="text" name="country" size="45px"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Postal Code</td>
+                                <td>-</td>
+                                <td><input type="text" name="postal" size="20px" onchange="verify_postal()"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Home Phone Number</td>
+                                <td>-</td>
+                                <td><input type="text" name="home_phone" size="20px" onchange="verify_home_phone()"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Business Phone Number</td>
+                                <td>-</td>
+                                <td><input type="text" name="business_phone" size="20px" onchange="verify_business_phone()"></td>
+                            </tr>
+                            <tr>
+                                <td class="alignRight">Email</td>
+                                <td>-</td>
+                                <td><input type="text" name="email" size="45px" onchange="verify_email()"></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><input type="checkbox" name="accept_terms" onchange="verify_accept_terms()"> 
+                                    Yes, I accept the terms:
+                                </td>
+                            </tr>
+                        </table>
+                    </div02>
+    
+                    <br /><br /><br /><br /><br />
+                    
+                    <!--- Row Two --->
+                    <div02>
+                        <p style="color:cyan"><b>New Profile Information</b></p>
+                        <hr>
+                        <table>
+                            <col width="200px">
+                            <col width="15px">
+                            <col width="350px">
+
+                            <tr> 
+                                <td class="alignRight">User ID</td>
+                                <td>-</td>
+                                <td><input type="text" name="user_id" size="45px"></td>
+                            </tr>
+                            <tr> 
+                                <td class="alignRight">Password</td>
+                                <td>-</td>
+                                <td><input type="text" name="password" size="45px" onchange="verify_password()"></td>
+                            </tr>
+                            <tr> 
+                                <td colspan="3">select a password between 3-8 characters</td>
+                            </tr>
+                        </table>
+                    </div02>
+                    
+                    <!--- Row Three -->
+                    <div02>
+                        <p style="color:cyan" ><b>Additonal Information</b></p>
+                        <hr>
+                        <table >
+                            <col width="200px">
+                            <col width="15px">
+                            <col width="350px">
+
+                            <tr> 
+                                <td class="alignRight">Your Message</td>
+                                <td>-</td>
+                                <td>
+                                    <textarea name="comments" cols="40" rows="8">Enter message here
+                                    </textarea>
+                                </td>
+                            </tr>   
+                        </table>
+                    </div02>
+
+                    <!---Submit-Reset and Home Buttons --->
+                    <div02 style="float:left; background-color:transparent;"> 
+                        <input type="submit" onclick="return confirm('Do you want to submit the information?')" style="width:100px; height:25px;">
+                        <br /><br />
+       
+                        <input type="reset"  onclick="return confirm('Do you want to reset all?')" style="width:100px; height:25px;">
+                        <br /><br />
+                        
+                        <a href="index.php">
+                        <img src="Images/home.png" alt="return home" style="width:100px; height:25px;"></a>
+                    </div02>
+                </form>
         
             </div><!-- section ends --> 
             
