e21633c36da18e0c55808894e133787203f21799
Leisy Moliner <lhmoliner@gmail.com>
2014-11-12 13:54:20 -0700

    index page and navigation
    buttons, CSS, etc

--- navigation.php
+++ navigation.php
@@ -1,8 +1,34 @@
 <!DOCTYPE html>
 <html>
 
-    <head></head>
+    <head>
+        <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'><!--google fonts-->
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"><!--css stylesheet-->
+        <script type="text/javascript" src="javaScriptInfo.js"></script><!--javascript functions-->
+        
+        <!--Menu/Navigation seccion to be loaded from every page-->
+        <title>Navigation</title>
+    </head>
     <body>
         <!--<br /><h1>welcome to Travel Experts</h1>-->
+        <!--loading buttons for the side menu-->
+        <table border="1px">
+            <col width="150px">
+            <col width="150px">
+            <col width="150px">
+            <col width="150px">
+            <col width="150px">
+            <col width="300px">
+                 
+            <tr>
+                <td><a href="index.php" id="buttonNav">Home</a></td>
+                <td><a href="contact.php" id="buttonNav">Contact Us</a></td>
+                <td><a href="register.php" id="buttonNav">Register</a></td>
+                <td><a href="links.php" id="buttonNav">Links</a></td>
+                <td><a href="login.php" id="buttonNav">Agents</a></td>
+                <td><a href="login.php" id="buttonSpecial">Login</a></td>
+            </tr>
+        </table>
+            
     </body>
 </html>
