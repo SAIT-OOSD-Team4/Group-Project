330902e02f1c8cf063b1edd50648ea7ac6d22512
jasmeenk <jasmeenjaasz@gmail.com>
2014-11-13 12:56:08 -0700

    icons code added
    icons code added

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
 
-    <head></head>
+    <head>
+			<!--- Social Media Icons Code --->
+			<script type="text/javascript">var switchTo5x=true;</script>
+			<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+			<script type="text/javascript">stLight.options({publisher: "c4a76f89-6db1-4c45-9422-6eb5b519304d", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+			<!----- Socail Media Icons Code Ends -------->
+			
+	
+	
+	</head>
     <body>
         Copyright Team4 &copy; 2014<br>
                 <script>
@@ -10,6 +19,19 @@
                     document.write("<br>");
                     document.write(days[today.getDay()]+", ");/*display date on footer*/
                     document.write(months[today.getMonth()].toUpperCase()+" "+today.getDate()+", "+today.getFullYear());
+					
                 </script>  
+				
+				<!-- Social Media Icons Code  -->
+				
+				<span class='st_sharethis_large' displayText='ShareThis'></span>
+				<span class='st_facebook_large' displayText='Facebook'></span>
+				<span class='st_twitter_large' displayText='Tweet'></span>
+				<span class='st_linkedin_large' displayText='LinkedIn'></span>
+				<span class='st_pinterest_large' displayText='Pinterest'></span>
+				<span class='st_email_large' displayText='Email'></span>
+				
+				
+				<!-- Social Media Icons Code Ends -->
     </body>
 </html>
